01b7d553e35a132c2a19e768344e1aafa9c94b25
알파 <arpa1705@gmail.com>
2024-02-19 21:04:43 +0900

    feat: macos 환경에서 window 사이즈 수정

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -35,87 +35,93 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Expanded(child: SizedBox()),
-            const Text(
-              '환영합니다!',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              '시작하기 위해 먼저 BOJ 아이디로 로그인해주세요.',
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            const SizedBox(height: 20),
-            Form(
-              key: _formKey,
+            Container(
+              constraints: BoxConstraints(maxWidth: 500),
               child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '아이디를 입력하세요.';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: const Color.fromRGBO(235, 236, 243, 1),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      isDense: true,
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 10,
-                      ),
-                      hintText: "Baekjoon OJ 아이디",
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '환영합니다!',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
                     ),
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    onFieldSubmitted: (text) async {
-                      final SharedPreferences prefs =
-                          await SharedPreferences.getInstance();
-
-                      await prefs.setString('bojId', text);
-
-                      handleLogin();
-                    },
-                    textInputAction: TextInputAction.go,
                   ),
-                  const SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: handleLogin,
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                              Theme.of(context).primaryColor,
+                  const SizedBox(height: 4),
+                  Text(
+                    '시작하기 위해 먼저 BOJ 아이디로 로그인해주세요.',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  const SizedBox(height: 20),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '아이디를 입력하세요.';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: const Color.fromRGBO(235, 236, 243, 1),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
+                            isDense: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 10,
                             ),
+                            hintText: "Baekjoon OJ 아이디",
                           ),
-                          child: const Text(
-                            '로그인',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onFieldSubmitted: (text) async {
+                            final SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+
+                            await prefs.setString('bojId', text);
+
+                            handleLogin();
+                          },
+                          textInputAction: TextInputAction.go,
                         ),
-                      )
-                    ],
-                  )
+                        const SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: handleLogin,
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    Theme.of(context).primaryColor,
+                                  ),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                                child: const Text(
+                                  '로그인',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
